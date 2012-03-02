fc8a9b31cd22495cd6f9076e5b200ec88dc7f87d
Federico Martinez <southworks-qa@live.com>
2012-03-02 09:56:01 -0300

    removed some files

--- Source/Ex3-WindowsAzureDeployment/VB/Begin/GuestBook_WebRole/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.vb" Inherits="GuestBook_WebRole.Global_asax" Language="vb" %>
--- Source/Setup/Cleanup.cmd
+++ Source/Setup/Cleanup.cmd
@@ -6,7 +6,6 @@ echo ======================================================
 echo.
 
 DEL "%USERPROFILE%\Documents\Visual Studio 2010\Code Snippets\Visual C#\My Code Snippets\IntroductiontoWindowsAzureEx*.snippet"
-DEL "%USERPROFILE%\Documents\Visual Studio 2010\Code Snippets\Visual Basic\My Code Snippets\IntroductiontoWindowsAzureEx*.snippet"
 DEL "%USERPROFILE%\Documents\Visual Studio 2010\Code Snippets\XML\My Xml Snippets\IntroductiontoWindowsAzureEx*.snippet"
 
 echo Lab Code Snippets have been removed!
